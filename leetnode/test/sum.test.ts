a352bb8ecafc9b2be6b9623202019fb81943030c
ace2light <tamlina1996@gmail.com>
2023-10-30 12:01:30 +0800

    Revert "I added test code config files."
    This reverts commit cf973fdb79c909eb5fc0ad7f455cbef0633e52c0.

--- leetnode/test/sum.test.ts
+++
@@ -1,7 +0,0 @@
-import sum from '../helpers/sum';
-
-describe('The sum function should work as expected', () => {
-  it('should sum numbers correctly', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
