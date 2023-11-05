9b51088dfc1b32cdf4e13735a45e2b24020ee1d0
Hung Tien <leehungtien@gmail.com>
2023-11-05 17:02:19 +0800

    Fix for Leaderboard search bar test case

--- leetnode/src/components/user/Leaderboard.test.tsx
+++ leetnode/src/components/user/Leaderboard.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor, } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import Leaderboard from './Leaderboard';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import userEvent from '@testing-library/user-event'
@@ -39,16 +39,52 @@ test('renders Leaderboard component', async () => {
     const searchInput = screen.queryByPlaceholderText(/Search User/);
     if (searchInput) {
       expect(searchInput).toBeInTheDocument();
-  
-      // // Simulate user interaction (example: changing the search input)
-      // const user = userEvent.setup()
-      // user.type(searchInput, 'Test User');
-
-      // // Validate the component's response
-      // expect(screen.getByText('Test User')).toBeTruthy();
     }
-
-    // Verify snapshots
-    // expect(screen.container.firstChild).toMatchSnapshot();
   });
 });
+
+test('Test on search bar', async () => {
+  const mockData = [
+    {
+      id: '1',
+      username: 'user1',
+      points: 100,
+      image: null,
+    },
+    {
+      id: '2',
+      username: 'user2',
+      points: 150,
+      image: null,
+    },
+  ];
+
+  // Mock the useQuery hook to return the mock data
+  jest.spyOn(queryClient, 'getQueryData').mockReturnValue(mockData);
+
+  render(
+    <QueryClientProvider client={queryClient}>
+      <Leaderboard />
+    </QueryClientProvider>
+  );
+
+  // Wait for the component to load
+  await waitFor(() => {
+    // Check for specific content
+    const searchInput = screen.queryByPlaceholderText(/Search User/);
+    if (searchInput) {
+      expect(searchInput).toBeInTheDocument();
+
+      // Simulate user interaction (example: changing the search input)
+      fireEvent.change(searchInput, { target: { value: 'user1' } });
+    }
+
+    // Wait for the component to update
+    waitFor(() => {
+      // Validate the component's response
+      expect(screen.getByText('user1')).toBeInTheDocument();
+      expect(screen.queryByText('user2')).not.toBeInTheDocument();
+    });
+  });
+});
+
