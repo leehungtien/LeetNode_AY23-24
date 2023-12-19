e7ff50a90454ac59c5e54237be4c2b39e5c42583
ace2light <tamlina1996@gmail.com>
2023-12-19 15:24:22 +0800

    add more packages

--- leetnode/jest.config.ts
+++ leetnode/jest.config.ts
@@ -1,12 +1,17 @@
 export default {
-    testEnvironment: "jsdom",
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-    setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-    moduleNameMapper: {
-      "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    },
-    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-      "/^.+.(css|less|scss|sass)$/": "identity-obj-proxy",
-    },
-  };
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/src/pages/test.tsx",
+  ],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    //"^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "/^.+.(css|less|scss|sass)$/": "identity-obj-proxy",
+  },
+};
