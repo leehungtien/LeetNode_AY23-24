2704b4ecccf547ddbf568915c09ce1860fbdd790
ace2light <tamlina1996@gmail.com>
2024-02-08 16:47:40 +0800

    "button" made in questions page but not functioning yet

--- leetnode/src/components/course/PracticeQuestion.tsx
+++ leetnode/src/components/course/PracticeQuestion.tsx
@@ -29,6 +29,13 @@ import {
 import { Question, QuestionWithAddedTime, User } from "@prisma/client";
 import { IconBulb } from "@tabler/icons";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+// import highlighter icon 
+//import { IconHighlighted, IconNotHighlighted } from 'icon-library';
+import IconHighlighted from './IconHighlighted.png';
+import IconNotHighlighted from './IconNotHighlighted.png';
+import Image from 'next/image';
+
+
 
 interface UserData extends User {
   attempts: { [timestamp: string]: number };
@@ -50,6 +57,13 @@ export default function PracticeQuestion() {
 
   const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
   const [hintsOpened, setHintsOpened] = useState<boolean>(false);
+  // define a state variable highlightActive using the useState hook 
+  const [highlightActive, setHighlightActive] = useState<boolean>(false);
+  
+  // initialize highlightActive 
+  const toggleHighlight = () => {
+    setHighlightActive((prev) => !prev);
+  };
 
   const { data: UCQAT } = useQuery({
     queryKey: ["get-ucqat"],
@@ -199,163 +213,177 @@ export default function PracticeQuestion() {
 
   return (
     <Paper p="xl" radius="md" withBorder>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (selectedKeys.length === 0) {
-            toast.error("Please select an option");
-            return;
-          }
-          submitAnswer({
-            query: {
-              qatId: UCQAT.data.qatId,
-              courseSlug: currentCourseSlug,
-            },
-            body: {
-              attemptedKeys: selectedKeys,
-              isCorrect:
-                selectedKeys.length === correctKeys.length &&
-                selectedKeys.every((item) => correctKeys.includes(item)),
-              topicSlug: UCQAT.data.question.topicSlug,
-              topicName: UCQAT.data.question.topic.topicName,
-            },
-          });
-        }}
-      >
-        <QuestionDifficultyBadge
-          questionDifficulty={UCQAT.data.question.questionDifficulty}
-          {...{ radius: "lg", size: "md" }}
-        />
-        <div
-          className="rawhtml mt-4"
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(UCQAT.data.question.questionContent, {
-              ADD_TAGS: ["iframe"],
-              ADD_ATTR: [
-                "allow",
-                "allowfullscreen",
-                "frameborder",
-                "scrolling",
-              ],
-            }),
-          }}
-        />
-        <VariablesBox
-          variables={UCQAT.data.variables as QuestionDataType["variables"]}
-        />
-        {correctKeys.length === 1 ? (
-          <Radio.Group
-            mt="xl"
-            value={selectedKeys[0]}
-            onChange={(value) => {
-              console.log(
-                value === answerOptions.find((item) => item.isCorrect)?.key
-              );
-              setSelectedKeys([value]);
-            }}
-            orientation="vertical"
-            description="Select only one option"
-            required
-          >
-            {answerOptions.map((item) => (
-              <Radio
-                key={item.key}
-                value={item.key}
-                label={
-                  item.isLatex ? (
-                    <Latex>{`$$ ${item.answerContent} $$`}</Latex>
-                  ) : (
-                    <Text>{item.answerContent}</Text>
-                  )
-                }
-                className={`flex items-center justify-start rounded-md border border-solid ${
-                  theme.colorScheme === "dark"
-                    ? "border-zinc-600 bg-zinc-700"
-                    : "border-gray-200 bg-gray-100"
-                } p-2`}
-              />
-            ))}
-          </Radio.Group>
-        ) : (
-          <Checkbox.Group
-            mt="xl"
-            value={selectedKeys}
-            onChange={(values) => {
-              console.log(
-                values.length === correctKeys.length &&
-                  values.every((item) => correctKeys.includes(item))
-              );
-              setSelectedKeys(values);
-            }}
-            orientation="vertical"
-            description="Select all correct options"
-            required
-          >
-            {answerOptions.map((item) => (
-              <Checkbox
-                key={item.key}
-                value={item.key}
-                label={
-                  item.isLatex ? (
-                    <Latex>{item.answerContent}</Latex>
-                  ) : (
-                    <Text>{item.answerContent}</Text>
-                  )
-                }
-                className={`flex items-center justify-start rounded-md border border-solid ${
-                  theme.colorScheme === "dark"
-                    ? "border-zinc-600 bg-zinc-700"
-                    : "border-gray-200 bg-gray-100"
-                } p-2`}
-              />
-            ))}
-          </Checkbox.Group>
-        )}
-        <Flex mt="xl" align="center" gap="md">
-          <Button
-            type="submit"
-            variant="light"
-            fullWidth
-            loading={submitAnswerStatus === "loading"}
-          >
-            {submitAnswerStatus === "loading" ? "Submitting..." : "Submit"}
-          </Button>
-          {(UCQAT.data.question.questionData as QuestionDataType).hints && (
-            <Tooltip label="Hints" withArrow>
-              <ActionIcon
-                size="lg"
-                variant="light"
-                radius="xl"
-                onClick={() => setHintsOpened(true)}
-              >
-                <IconBulb size={20} />
-              </ActionIcon>
-            </Tooltip>
-          )}
-        </Flex>
-
-        {/* Hints Modal */}
-        <Modal
-          opened={hintsOpened}
-          onClose={() => setHintsOpened(false)}
-          title="Hints"
-          size="md"
+      <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '1rem' }}>
+        <Button
+          variant={highlightActive ? 'filled' : 'light'}
+          color={highlightActive ? 'dark' : 'gray'}
+          onClick={toggleHighlight}
         >
-          <Stack>
-            {(UCQAT.data.question.questionData as QuestionDataType).hints?.map(
-              (item, index) => (
-                <Box
-                  key={index}
-                  className="flex items-center justify-start gap-3 rounded-md border border-solid border-gray-200 bg-gray-100 p-2"
+          {highlightActive ? (
+            <Image src={IconHighlighted} alt="IconHighlighted" width={100} height={100} />
+          ) : (
+            <Image src={IconNotHighlighted} alt="IconNotHighlighted" width={100} height={100} />
+          )}
+          <span>{highlightActive ? 'Deactivate Highlighter' : 'Activate Highlighter'}</span>
+        </Button>
+      </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (selectedKeys.length === 0) {
+              toast.error("Please select an option");
+              return;
+            }
+            submitAnswer({
+              query: {
+                qatId: UCQAT.data.qatId,
+                courseSlug: currentCourseSlug,
+              },
+              body: {
+                attemptedKeys: selectedKeys,
+                isCorrect:
+                  selectedKeys.length === correctKeys.length &&
+                  selectedKeys.every((item) => correctKeys.includes(item)),
+                topicSlug: UCQAT.data.question.topicSlug,
+                topicName: UCQAT.data.question.topic.topicName,
+              },
+            });
+          }}
+        >
+          <QuestionDifficultyBadge
+            questionDifficulty={UCQAT.data.question.questionDifficulty}
+            {...{ radius: "lg", size: "md" }}
+          />
+          <div
+            className="rawhtml mt-4"
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(UCQAT.data.question.questionContent, {
+                ADD_TAGS: ["iframe"],
+                ADD_ATTR: [
+                  "allow",
+                  "allowfullscreen",
+                  "frameborder",
+                  "scrolling",
+                ],
+              }),
+            }}
+          />
+          <VariablesBox
+            variables={UCQAT.data.variables as QuestionDataType["variables"]}
+          />
+          {correctKeys.length === 1 ? (
+            <Radio.Group
+              mt="xl"
+              value={selectedKeys[0]}
+              onChange={(value) => {
+                console.log(
+                  value === answerOptions.find((item) => item.isCorrect)?.key
+                );
+                setSelectedKeys([value]);
+              }}
+              orientation="vertical"
+              description="Select only one option"
+              required
+            >
+              {answerOptions.map((item) => (
+                <Radio
+                  key={item.key}
+                  value={item.key}
+                  label={
+                    item.isLatex ? (
+                      <Latex>{`$$ ${item.answerContent} $$`}</Latex>
+                    ) : (
+                      <Text>{item.answerContent}</Text>
+                    )
+                  }
+                  className={`flex items-center justify-start rounded-md border border-solid ${
+                    theme.colorScheme === "dark"
+                      ? "border-zinc-600 bg-zinc-700"
+                      : "border-gray-200 bg-gray-100"
+                  } p-2`}
+                />
+              ))}
+            </Radio.Group>
+          ) : (
+            <Checkbox.Group
+              mt="xl"
+              value={selectedKeys}
+              onChange={(values) => {
+                console.log(
+                  values.length === correctKeys.length &&
+                    values.every((item) => correctKeys.includes(item))
+                );
+                setSelectedKeys(values);
+              }}
+              orientation="vertical"
+              description="Select all correct options"
+              required
+            >
+              {answerOptions.map((item) => (
+                <Checkbox
+                  key={item.key}
+                  value={item.key}
+                  label={
+                    item.isLatex ? (
+                      <Latex>{item.answerContent}</Latex>
+                    ) : (
+                      <Text>{item.answerContent}</Text>
+                    )
+                  }
+                  className={`flex items-center justify-start rounded-md border border-solid ${
+                    theme.colorScheme === "dark"
+                      ? "border-zinc-600 bg-zinc-700"
+                      : "border-gray-200 bg-gray-100"
+                  } p-2`}
+                />
+              ))}
+            </Checkbox.Group>
+          )}
+          <Flex mt="xl" align="center" gap="md">
+            <Button
+              type="submit"
+              variant="light"
+              fullWidth
+              loading={submitAnswerStatus === "loading"}
+            >
+              {submitAnswerStatus === "loading" ? "Submitting..." : "Submit"}
+            </Button>
+            {(UCQAT.data.question.questionData as QuestionDataType).hints && (
+              <Tooltip label="Hints" withArrow>
+                <ActionIcon
+                  size="lg"
+                  variant="light"
+                  radius="xl"
+                  onClick={() => setHintsOpened(true)}
                 >
-                  <Text color="dimmed">#{index + 1}</Text>
-                  <Text>{item.hint}</Text>
-                </Box>
-              )
+                  <IconBulb size={20} />
+                </ActionIcon>
+              </Tooltip>
             )}
-          </Stack>
-        </Modal>
-      </form>
-    </Paper>
-  );
-}
+          </Flex>
+
+          {/* Hints Modal */}
+          <Modal
+            opened={hintsOpened}
+            onClose={() => setHintsOpened(false)}
+            title="Hints"
+            size="md"
+          >
+            <Stack>
+              {(UCQAT.data.question.questionData as QuestionDataType).hints?.map(
+                (item, index) => (
+                  <Box
+                    key={index}
+                    className="flex items-center justify-start gap-3 rounded-md border border-solid border-gray-200 bg-gray-100 p-2"
+                  >
+                    <Text color="dimmed">#{index + 1}</Text>
+                    <Text>{item.hint}</Text>
+                  </Box>
+                )
+              )}
+            </Stack>
+          </Modal>
+        </form>
+      </Paper>
+    );
+  }
