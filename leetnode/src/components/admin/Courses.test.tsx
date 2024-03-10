5c7fa182180112c175f9ec1a8b5977fb924f601e
ace2light <tamlina1996@gmail.com>
2024-03-10 17:17:46 +0800

    add Courses.test.tsx (38.5% stmt coverage)

---
+++ leetnode/src/components/admin/Courses.test.tsx
@@ -0,0 +1,75 @@
+// Mock ResizeObserver
+global.ResizeObserver = class ResizeObserver {
+    callback: any;
+    constructor(callback: any) {
+      this.callback = callback;
+    }
+    observe() {
+      // Do nothing
+    }
+    unobserve() {
+      // Do nothing
+    }
+    disconnect() {
+      // Do nothing
+    }
+  };
+  import { render, screen, waitFor } from '@testing-library/react';
+  import axios from 'axios';
+  import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+  import Courses from './Courses';
+  // Mock axios and external libraries
+  jest.mock('axios');
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  jest.mock('rehype-katex', () => () => 'katex');
+  jest.mock('rehype-raw', () => () => 'raw');
+  jest.mock('remark-math', () => () => 'math');
+  // Setup QueryClient for tests
+  const createTestQueryClient = () => new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+  // Utility function to wrap component with necessary providers
+  const renderWithProviders = (component: React.ReactElement) => {
+    const TestQueryClient = createTestQueryClient();
+    return render(
+      <QueryClientProvider client={TestQueryClient}>
+        {component}
+      </QueryClientProvider>
+    );
+  };
+  describe('Courses Component', () => {
+    beforeEach(() => {
+      // Reset all mocks before each test
+      jest.clearAllMocks();
+      // Mock API calls
+      mockedAxios.get.mockImplementation(url => {
+        switch (url) {
+          case "/api/course":
+            return Promise.resolve({ data: [{ courseSlug: "course-1", courseName: "Course 1", courseDescription: "Description for Course 1", courseLevel: "Beginner" }] });
+          case "/api/user/admin":
+          case "/api/attempt/admin":
+          case "/api/question/admin":
+            return Promise.resolve({ data: [] });
+          default:
+            return Promise.reject(new Error("not found"));
+        }
+      });
+    });
+    
+    it('renders correctly', async () => {
+      renderWithProviders(<Courses />);
+      // Await the removal of the progress bar, which indicates loading has finished
+      await waitFor(() => expect(screen.queryByRole('progressbar')).not.toBeInTheDocument());
+      // Then check for static text known to be in the component to verify render
+      await waitFor(() => expect(screen.getByText("All Courses")).toBeInTheDocument());
+      // Check for dynamically loaded content
+      await waitFor(() => expect(screen.getByText("Course 1")).toBeInTheDocument());
+      // Check if axios was called correctly
+      expect(mockedAxios.get).toHaveBeenCalledWith("/api/course");
+    });
+  });
+  
