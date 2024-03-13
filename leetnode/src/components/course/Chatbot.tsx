0ce485e68a62fc05b3a67400950bc46ec3a0b8fa
Hung Tien <leehungtien@gmail.com>
2024-03-13 22:32:30 +0800

    Multi-modal with conversation history

--- leetnode/src/components/course/Chatbot.tsx
+++ leetnode/src/components/course/Chatbot.tsx
@@ -1,29 +1,24 @@
 import { useEffect, useRef, useState } from "react";
 import { useDisclosure } from '@mantine/hooks';
 import { Dialog, Group, Button, Text, ScrollArea, Textarea, Box, Loader, FileInput } from '@mantine/core';
-// import 'dotenv/config';
-import { HarmBlockThreshold, HarmCategory, TaskType } from "@google/generative-ai";
-import { ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
-import { PromptTemplate, ChatPromptTemplate } from "@langchain/core/prompts";
+
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { HumanMessage } from "@langchain/core/messages";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
-import { RunnableSequence, RunnablePassthrough } from "@langchain/core/runnables";
-import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { formatDocumentsAsString } from "langchain/util/document";
-import { createRetrieverTool } from "langchain/tools/retriever";
-import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
-import { pull } from "langchain/hub";
-import { createOpenAIFunctionsAgent, AgentExecutor } from "langchain/agents";
+
+interface Message {
+    role: 'You' | 'Bot';
+    parts: string;
+}
 
 const Chatbot = () => {
-    const [input, setInput] = useState<string>("");
-    const [reply, setReply] = useState<string>("");
-    const [opened, { toggle, close }] = useDisclosure(false);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [chatHistory, setChatHistory] = useState<string[]>([
-        "Hi, how can I be of service to you?",
-    ]);
+    const [prompt, setPrompt] = useState<string>("")
+    const [reply, setReply] = useState<string>("")
+    const [fullChat, setFullChat] = useState<Message[]>([{ role: 'Bot', parts: 'Hi, how can I be of service to you?' }])
+    const [opened, { toggle, close }] = useDisclosure(false)
+    const [loading, setLoading] = useState<boolean>(false)
 
     //Scroll to bottom after every response
     const messagesEndRef = useRef<HTMLDivElement>(null)
@@ -32,13 +27,13 @@ const Chatbot = () => {
     }
     useEffect(() => {
         scrollToBottom()
-    }, [reply, chatHistory]);
+    }, [reply, fullChat]);
 
     //Save the user input
-    const inputChange = (e: any) => {
+    const promptChange = (e: any) => {
         e.preventDefault();
 
-        setInput(e.target.value);
+        setPrompt(e.target.value);
     }
 
     //LLM (Langchain + Gemini AI)
@@ -48,7 +43,20 @@ const Chatbot = () => {
         setReply("");
 
         const outputParser = new StringOutputParser();
-        const updatedChat: string[] = [...chatHistory];
+        const updatedChat: Message[] = [...fullChat];
+
+        //Image conversion to Base64
+        const toBase64 = (file: File) => new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = (error) => reject(error);
+        });
+        const fileInputEl = document.querySelector("input[type=file]") as HTMLInputElement;
+        const imageParts = await Promise.all(
+            [...fileInputEl.files as FileList].map(toBase64)
+        );
+        const typedImageParts = imageParts as { inlineData: { data: string; mimeType: string; }; }[];
 
         // LLM config
         const safetySettings = [
@@ -70,136 +78,67 @@ const Chatbot = () => {
             }
         ];
         const llm = new ChatGoogleGenerativeAI({
-            modelName: "gemini-pro",
+            modelName: fileInputEl.files?.length ? "gemini-pro-vision" : "gemini-pro",
             maxOutputTokens: 500,
             safetySettings,
             cache: true,
             temperature: 0.5,
-            streaming: true,
             apiKey: "AIzaSyCaV5djusWE31J5Atgd71eqpHhN6Uwmy2E"
         });
 
-        const splitter = new RecursiveCharacterTextSplitter({
-            chunkSize: 1000,
-            chunkOverlap: 200
-        });
-        const loader = new CheerioWebBaseLoader(
-            "https://js.langchain.com/docs/get_started/introduction"
-        );
-        const docs = await loader.load();
-        const splitDocs = await splitter.splitDocuments(docs);
-        const embeddings = new GoogleGenerativeAIEmbeddings({
-            modelName: "embedding-001", // 768 dimensions
-            taskType: TaskType.RETRIEVAL_DOCUMENT,
-            title: "LeetNode",
-            apiKey: "AIzaSyCaV5djusWE31J5Atgd71eqpHhN6Uwmy2E"
-        });
-        const vectorstore = await MemoryVectorStore.fromDocuments(
-            splitDocs,
-            embeddings
-        );
-        const retriever = vectorstore.asRetriever();
+        //Prompt templates
+        const systemTemplate = `You are a professor in a prestigious university. Input: {input}`
 
-        const formatChatHistory = (messages: string[]) => {
-            return messages.map((message, i) => {
-                if (i % 2 === 0) {
-                    return `You: ${message}`
-                }
-                else { return `Bot: ${message}` }
-            }).join('\n')
-        }
+        const input =
+            new HumanMessage({
+                content: fileInputEl.files?.length
+                    ? [
+                        {
+                            type: "text",
+                            text: prompt,
+                        },
+                        {
+                            type: "image_url",
+                            image_url: `${typedImageParts}`,
+                        },
+                    ]
 
-        const questionPrompt = PromptTemplate.fromTemplate(
-            `Use the following pieces of context to answer the question at the end. Look at the chatHistory first to have context. If you don't know the answer, use Tavily to search the web, don't try to make up an answer.
-            ----------------
-            CONTEXT: {context}
-            ----------------
-            CHAT HISTORY: {chatHistory}
-            ----------------
-            QUESTION: {question}
-            ----------------
-            Helpful Answer:`
-        );
-          
-        const chain = RunnableSequence.from([
-            {
-              question: (input: { question: string; chatHistory?: string }) =>
-                input.question,
-              chatHistory: (input: { question: string; chatHistory?: string }) =>
-                input.chatHistory ?? "",
-              context: async (input: { question: string; chatHistory?: string }) => {
-                const relevantDocs = await retriever.getRelevantDocuments(input.question);
-                const serialized = formatDocumentsAsString(relevantDocs);
-                return serialized;
-              },
-            },
-            questionPrompt,
-            llm,
-            new StringOutputParser(),
+                    : [
+                        {
+                            type: "text",
+                            text: prompt,
+                        },
+                    ],
+            });
+
+        const chatPromptChain = ChatPromptTemplate.fromMessages([
+            ["system", systemTemplate],
+            input
         ]);
 
-        const res = await chain.invoke({
-            chatHistory: formatChatHistory(chatHistory),
-            question: input,
+        const chain = chatPromptChain.pipe(llm).pipe(outputParser);
+
+        const transformFullChatToInputFormat = (fullChat: Message[]) => {
+            return fullChat.map(msg => {
+                return { type: "text", text: `${msg.role}: ${msg.parts}` };
+            });
+        };
+
+        const fullChatFormatted = transformFullChatToInputFormat(fullChat);
+        const combinedInput = [...fullChatFormatted, ...input.content];
+        const res = await chain.stream({
+            input: combinedInput
         });
 
-        const retrieverTool = await createRetrieverTool(retriever, {
-            name: "langsmith_search",
-            description:
-              "Search for information about LangSmith. For any questions about LangSmith, you must use this tool!",
-        });
-
-        const searchTool = new TavilySearchResults({
-            apiKey: "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"
-        });
-        // const tools = [retrieverTool, searchTool];
-        const tools = [searchTool];
-
-        // Get the prompt to use - you can modify this!
-        // If you want to see the prompt in full, you can at:
-        // https://smith.langchain.com/hub/hwchase17/openai-functions-agent
-        const agentPrompt = await pull<ChatPromptTemplate>(
-            "hwchase17/openai-functions-agent"
-        );
-
-        const agent = await createOpenAIFunctionsAgent({
-        llm: llm,
-        tools,
-        prompt: agentPrompt,
-        });
-
-        const agentExecutor = new AgentExecutor({
-        agent,
-        tools,
-        verbose: true,
-        });
-
-        // const agentResult = await agentExecutor.invoke({
-        //     input: "how can LangSmith help with testing?",
-        // });
-          
-        // console.log(agentResult.output);
-
-        const agentResult = await agentExecutor.invoke({
-            input: formatChatHistory(chatHistory) + input,
-        });
-          
-        // console.log('agent2 reply: ' + agentResult2.output);
-          
         let text = '';
-        for await (const chunk of agentResult.output) {
+        for await (const chunk of res) {
             text += chunk;
             setReply(text);
         };
-        // let text = '';
-        // for await (const chunk of res) {
-        //     text += chunk;
-        //     setReply(text);
-        // };
 
-        updatedChat.push(input);
-        updatedChat.push(text);
-        setChatHistory(updatedChat);
+        updatedChat.push({ role: 'You', parts: prompt });
+        updatedChat.push({ role: 'Bot', parts: text });
+        setFullChat(updatedChat);
         setLoading(false);
     }
 
@@ -222,13 +161,13 @@ const Chatbot = () => {
                             </p>
                         </Box>
 
-                        : chatHistory.map((msg) => (
-                            <Box key={chatHistory.indexOf(msg)} className="mt-5">
-                                <Text size="md" fw={600} color={chatHistory.indexOf(msg) % 2 == 0 ? "cyan" : "orange"}>
-                                    {chatHistory.indexOf(msg) % 2 == 0 ? "Bot" : "You"}
+                        : fullChat.map((msg) => (
+                            <Box key={msg.role} className="mt-5">
+                                <Text size="md" fw={600} color={msg.role == "Bot" ? "cyan" : "orange"}>
+                                    {msg.role}
                                 </Text>
                                 <p className="px-4 py-2 bg-neutral-100 rounded-md leading-8">
-                                    {msg}
+                                    {msg.parts}
                                 </p>
                             </Box>
                         ))}
@@ -245,9 +184,9 @@ const Chatbot = () => {
                         :
                         <Textarea minRows={4} maxRows={4}
                             placeholder="Type your prompt"
-                            value={input}
-                            onChange={(e) => inputChange(e)}
-                            onKeyDown={(e) => { if (e.key === "Enter") { chatlog(e); setInput(""); } }}
+                            value={prompt}
+                            onChange={(e) => promptChange(e)}
+                            onKeyDown={(e) => { if (e.key === "Enter") { chatlog(e); setPrompt(""); } }}
                             required
                             withAsterisk
                             className="w-full"
@@ -264,7 +203,7 @@ const Chatbot = () => {
                             :
                             <>
                                 <FileInput placeholder="Upload Image" clearable />
-                                <Button onClick={(e) => { chatlog(e); setInput(""); }}>
+                                <Button onClick={(e) => { chatlog(e); setPrompt(""); }}>
                                     Submit
                                 </Button>
                             </>
